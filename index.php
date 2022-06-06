f737b6f6a6304da606b2a1403dd7648f627449a6
Glenmhar Aurelio <89145761+Glenny07@users.noreply.github.com>
2022-06-06 11:15:53 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,239 @@
+<!-- 
+
+CREATED AND DESIGNED BY:
+---GLENMHAR PRIOL AURELIO--- 
+
+-->
+
+<?php include "includes/header.php" ?>
+<!-- NAVIGATION -->
+<body>
+ <main class="main">
+	<header>
+    	<nav class="navbar navbar-expand-lg navbar-light bg-transparent">
+        	<div class="container-fluid mt-1 m-5">
+             	<a class="navbar-brand brand" href="index.php"><img src="img/svg/logo.svg"  class="logo-brand" alt="logo" width="200" height="70"></a>
+                <button class="navbar-toggler collapse-nav" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                	<div class="me-auto"></div>
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="about.php">About Us</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="services.php">Services</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="careers.php">Careers</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="btn btn-success contact p-2" href="contact.php" role="button"><svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" fill="currentColor" class="bi bi-send" viewBox="0 0 16 16"><path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576 6.636 10.07Zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z"/></svg> Contact Us</a>
+                        </li>
+                    </ul>
+               	</div>
+            </div>
+       	</nav>
+    </header>
+    <!-- END OF NAVIGATION -->
+<!-- HERO SECTION -->
+        <div class="container mw-100">
+            <div class="row">
+                <!-- First Column -->
+                 <div class="col-lg-6 col-md-12 image-hero">
+                    <div class="hero-img">
+                        <img src="img/svg/hero.svg" class="img-fluid hero">
+                    </div>
+                 </div>
+                 <!-- Second Column -->
+                 <div class="col-lg-6 col-md-12">
+                    <div class="hero-tag fade-in-bottom">
+                        <h3 class="display-4">Find your fit and let's build success together</h3><br>
+                        <p class="lead">We have the best experts to elevate your business to the next level. We provide a variety of business services to companies in various industries.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <img class="mw-100 wave" src="img/svg/wave.svg" alt="Wave">
+        <img class="img-right" src="img/svg/hero-logo-right.svg" alt="Ashir Logo">
+</main>
+<!-- Back to top button -->
+<button class="back-to-top hidden"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#ffffff" class="bi bi-arrow-up-short" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M8 12a.5.5 0 0 0 .5-.5V5.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 5.707V11.5a.5.5 0 0 0 .5.5z"/>
+</svg></button>
+    <!-- END OF HERO SECTION -->
+    <!-- SECTION ONE -->
+
+    <section class="container section-one">
+        <!-- row-one -->
+        <div class="row">
+            <div class="col-lg-6 col-md-12 mt-4">
+                <div class="art-one" data-aos="fade-right">
+                    <img src="img/svg/section-one-art1.svg" class="img-fluid art-one-mobile" >
+                </div>
+            </div>
+            <div class="col-lg-6 col-md-12 mt-4">
+                <div class="first-half">
+                    <h3 class="display-6 pt-4">Who we are</h3>
+                    <p class="lead py-5">Ashir Business Process Outsource Corporation aims to engage in the business of process outsource services such as but not limited to accounting, bookkeeping, data encoding, payroll, tax compliance, social media marketing, telemarketing, customer support and other related services.</p>
+                </div>
+            </div>
+        </div>
+        <!-- row-two -->
+        <div class="row mt-5 mobile-parent">
+            <div class="col-lg-6 col-md-12 mt-4">
+                <div class="second-half">
+                    <h3 class="display-6 pt-4">What we do</h3>
+                    <p class="lead py-5">Ashir Business Process Outsource Corporation provides a variety of business services to companies in various industries. The firm focuses on accounting and tax compliance as well as business advisory and consultancy.</p>
+                </div>
+            </div>
+            <div class="col-lg-6 col-md-12 mt-1">
+                <div class="art-two" data-aos="fade-left">
+                    <img src="img/svg/section-one-art2.svg" class="img-fluid art-one-mobile">
+                </div>
+            </div>
+        </div>
+        <!-- row-three -->
+        <div class="row mt-5">
+            <div class="col-lg-6 col-md-12 mt-4">
+                <div class="art-one" data-aos="fade-right">
+                    <img src="img/svg/section-one-art3.svg" class="img-fluid art-one-mobile">
+                </div>
+            </div>
+            <div class="col-lg-6 col-md-12 mt-5">
+                <div class="third-half">
+                    <h3 class="display-6 pt-4">Why choose us</h3>
+                    <p class="lead py-5">Our staff comprise of a group of senior and junior associates and the firm also collaborate with a pool of specialized consultants in various fields of experience to perform consulting engagements and support the operation.</p>
+                </div>
+            </div>
+        </div>
+        <!-- row-four -->
+        <div class="row mt-5 mobile-parent">
+            <div class="col-lg-6 col-md-12 mt-5">
+                <div class="fourth-half">
+                    <h3 class="display-6 pt-4">Why join us</h3>
+                    <p class="lead py-5">Our partners advocate a continuous staff professional development through training programs to ensure and maintain highly qualified and competent staff that support the employee's career development through lifetime employment.</p>
+                </div>
+            </div>
+            <div class="col-lg-6 col-md-12 mt-4">
+                <div class="art-one art-join" data-aos="fade-left">
+                    <img src="img/svg/section-one-art4.svg" class="img-fluid art-one-mobile">
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- END OF SECTION ONE -->
+    <!-- SECTION TWO -->
+    <section class="section-two">
+        <div class="container">
+            <div class="section-name">
+                <h3 class="mt-5 pt-3 section-title" data-aos="zoom-in">Industry Capabilities and Expertise</h3>
+                <span class="underline"></span>
+                <div class="container section-two-para" data-aos="fade-up">
+                    <p>Based in the Philippines. Ashir Business Process Outsource Corporation employees are ready to help small businesses to grow and make big businesses better. Most are with degrees, multi-lingual and committed to delivering exceptional service.</p>
+                </div>
+            </div>
+            <div class="grid-container mt-3">
+                <div class="card-snippet shadow-pop-bl">
+                    <img src="img/icon/sprout.png" alt="Agriculture icon" width="50" height="50">
+                    <p>Agri - Aqua Business</p>
+                </div>
+                <div class="card-snippet shadow-pop-bl">
+                    <img src="img/icon/architecture.png" alt="Architectural icon" width="50" height="50">
+                    <p>Architectural & Interior</p>
+                </div>
+                <div class="card-snippet shadow-pop-bl">
+                    <img src="img/icon/bank.png" alt="bank icon" width="50" height="50">
+                    <p>Banking & Finance</p>
+                </div>
+                <div class="card-snippet shadow-pop-bl">
+                    <img src="img/icon/outsourcing.png" alt="BPO icon" width="50" height="50">
+                    <p>Business Process Outsource</p>
+                </div>
+                <div class="card-snippet shadow-pop-bl">
+                    <img src="img/icon/training.png" alt="training icon" width="50" height="50">
+                    <p>Training Institutions</p>
+                </div>
+                <div class="card-snippet shadow-pop-bl">
+                    <img src="img/icon/construction.png" alt="contruction icon" width="50" height="50">
+                    <p>Engineering & Construction</p>
+                </div>
+                <div class="card-snippet shadow-pop-bl">
+                    <img src="img/icon/air-freight.png" alt="Freight icon" width="50" height="50">
+                    <p>Freight Forwarding</p>
+                </div>
+                <div class="card-snippet shadow-pop-bl">
+                    <img src="img/icon/energy-saving.png" alt="Energy icon" width="50" height="50">
+                    <p>Gas, Power & Energy</p>
+                </div>
+                <div class="card-snippet shadow-pop-bl">
+                    <img src="img/icon/health-insurance.png	" alt="health icon" width="50" height="50">
+                    <p>Health, Beauty & Wellness</p>
+                </div>
+                <div class="card-snippet shadow-pop-bl">
+                    <img src="img/icon/hospital.png" alt="Hospital icon" width="50" height="50">
+                    <p>Hospitals & Clinics</p>
+                </div>
+                <div class="card-snippet shadow-pop-bl">
+                    <img src="img/icon/hotel-bell.png" alt="hotel icon" width="50" height="50">
+                    <p>Hotel & Restaurants</p>
+                </div>
+                <div class="card-snippet shadow-pop-bl">
+                    <img src="img/icon/configuration.png" alt="IT icon" width="50" height="50">
+                    <p>I.T. Related</p>
+                </div>
+                <div class="card-snippet shadow-pop-bl">
+                    <img src="img/icon/contract.png" alt="Insurance icon" width="50" height="50">
+                    <p>Insurance</p>
+                </div>
+                <div class="card-snippet shadow-pop-bl">
+                    <img src="img/icon/entertainment.png" alt="Entertainment icon" width="50" height="50">
+                    <p>Leisure & Entertainment</p>
+                </div>
+                <div class="card-snippet shadow-pop-bl">
+                    <img src="img/icon/construction-worker.png" alt="Manpower icon" width="50" height="50">
+                    <p>Manpower Services</p>
+                </div>
+                <div class="card-snippet shadow-pop-bl">
+                    <img src="img/icon/manufacture.png" alt="Manufacture icon" width="50" height="50">
+                    <p>Manufacturing & Export</p>
+                </div>
+                <div class="card-snippet shadow-pop-bl">
+                    <img src="img/icon/digital-advertising.png" alt="advertise icon" width="50" height="50">
+                    <p>Media & Advertising</p>
+                </div>
+                <div class="card-snippet shadow-pop-bl">
+                    <img src="img/icon/charity.png" alt="Non profit icon" width="50" height="50">
+                    <p>Non-profit Organization</p>
+                </div>
+                <div class="card-snippet shadow-pop-bl">
+                    <img src="img/icon/pills.png" alt="Pills icon" width="50" height="50">
+                    <p>Pharmaceuticals</p>
+                </div>
+                <div class="card-snippet shadow-pop-bl">
+                    <img src="img/icon/skyscraper.png" alt="Real estate icon" width="50" height="50">
+                    <p>Real Estate</p>
+                </div>
+                <div class="card-snippet shadow-pop-bl">
+                    <img src="img/icon/rent.png" alt="Rent icon" width="50" height="50">
+                    <p>Rental & Leasing</p>
+                </div>
+                <div class="card-snippet shadow-pop-bl">
+                    <img src="img/icon/money-flow.png" alt="Trading icon" width="50" height="50">
+                    <p>Trading, Distribution & Import</p>
+                </div>
+                <div class="card-snippet shadow-pop-bl">
+                    <img src="img/icon/logistics.png" alt="Transport icon" width="50" height="50">
+                    <p>Transport & Trucking</p>
+                </div>
+                <div class="card-snippet shadow-pop-bl">
+                    <img src="img/icon/backpack.png" alt="travel icon" width="50" height="50">
+                    <p>Travel & Tours</p>
+                </div>
+            </div>
+        </div>
+    </section>            
+    <!-- END OF SECTION TWO -->
+ <?php include "includes/footer.php" ?>
